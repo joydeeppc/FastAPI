b4ab096e1e2f1aa65c997a1ca31f7293d5dbfe6d
joydeep <joydeep.paulchoudhury99@gmail.com>
2024-02-28 21:55:12 +0530

    delete

--- app/__init__.py
+++
@@ -1,3 +0,0 @@
-# This is created because whenever we want to create a package we need to create an dummy file like this. we don't need to put anything in this file.        
-# package is nothing but a fancy name for folder
-
